cd179c4c389a1ee5b3dbb36238f90af72190fb5f
Olga <o.nikolaichenko@yandex.ru>
2023-08-24 18:06:35 +0300

    startGame version 1

--- game.js
+++ game.js
@@ -1,3 +1,16 @@
 "use strict";
 
 startGame();
+
+function startGame() {
+    const secretNumber = getSecretNumber();
+    while (true) {
+        let answerNumber = prompt(`Enter your answer please`);
+        let comparingResult = getCowsAndBullsCount();
+
+        if (result.bullsCount === 4) {
+            alert(`You won, the correct number is ${secretNumber}`);
+            break;
+        } else { `Your answer = ${answerNumber}, bulls: ${result.bullsCount}, cows: ${result.cowsCount}` };
+    }
+}
