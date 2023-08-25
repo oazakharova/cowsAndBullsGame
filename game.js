3ab11fea242c911a3ca58b7234c5618fb68da897
Olga <o.nikolaichenko@yandex.ru>
2023-08-25 12:46:06 +0300

    final version 1 is ready

--- game.js
+++ game.js
@@ -3,76 +3,68 @@
 startGame();
 
 function startGame() {
-    const secretNumber = getSecretNumber();
+    let secretNumber = getSecretNumber();
     while (true) {
-        let answerNumber = prompt(`Enter your answer please`);
-        let comparingResult = getCowsAndBullsCount();
+        let answerNumber = prompt(`Enter your answer please`).split("");
+        let comparingResult = getCowsAndBullsCount(secretNumber, answerNumber);
 
-        if (comparingResult.bullsCount === 4) {
+        if (secretNumber.toString() === answerNumber.toString()) {
             alert(`You won, the correct number is ${secretNumber}`);
             break;
-        } else { `Your answer = ${answerNumber}, bulls: ${comparingResult.bullsCount}, cows: ${comparingResult.cowsCount}` };
+        } else {
+            alert(`Your answer = ${answerNumber}, bulls: ${comparingResult.bullsCount}, cows: ${comparingResult.cowsCount}`);
+        };
     }
 }
 
 /**
  * Функция, возвращающая количество коров и быков
- * 
  */
-function getCowsAndBullsCount() {
-    let cowsCount = getCowsCount(riddle, answer);
-    let bullsCount = getBullsCount(riddle, answer);
-}
 
-function getCowsCount(riddle, answer) {
-    let cowsCount = 0;
-    for (let i = 0; i < riddle.length; i++) {
-        for (let j = 0; j < answer.length; j++) {
-            if (riddle[i] === answer[j] && i !== j) {
-                cowsCount++;
-            }
-        }
+function getCowsAndBullsCount(secretNumber, answerNumber) {
+    let cowsCount = getCowsCount(secretNumber, answerNumber);
+    let bullsCount = getBullsCount(secretNumber, answerNumber);
+    return {
+        "bullsCount": bullsCount,
+        "cowsCount": cowsCount - bullsCount,
     }
 }
 
-function getBullsCount(riddle, answer) {
+function getCowsCount(secretNumber, answerNumber) {
+    let cowsCount = 0;
+    for (let value of secretNumber) {
+        if (answerNumber.includes(value)) {
+            cowsCount++;
+        }
+    }
+    return cowsCount;
+}
+
+function getBullsCount(secretNumber, answerNumber) {
     let bullsCount = 0;
-    for (let i = 0; i < riddle.length; i++) {
-        if (riddle[i] === answer[i]) {
+    for (let i = 0; i < secretNumber.length; i++) {
+        if (secretNumber[i] == answerNumber[i]) {
             bullsCount++;
         }
     }
+    return bullsCount;
 }
+
 
 /**
  * Функция, которая генерирует случайные 4 цифры в виде массива
  * @returns массив из 4х случайных неповторяющихся цифр
  */
 function getSecretNumber() {
-    const min = 0;
-    const max = 9;
-    let secretNumber = [];
-    let count = 0;
-    for (let i = 0; i < 5; i++) {
-        let generatedNumber = getRandomInRange(min, max);
-        if (!generatedNumber in secretNumber) {
-            secretNumber = generatedNumber.push(secretNumber)
-            count++;
-        } else {
-            i--;
+    let generated = String(Math.random() * 1000000000000000);
+    let result = [];
+    for (let value of generated) {
+        if (!result.includes(value)) {
+            result.push(value);
         }
-        if (count === 4) {
-            return secretNumber;
+        if (result.length == 4) {
+            return result;
         }
     }
 }
 
-/**
- * Функция для получения рандомного целого числа в заданном диапазоне включая обе границы
- * @param {number} min - минимальная граница для получения рандомного числа
- * @param {number} max - максимальная граница для получения рандомного числа
- * @returns рандомное целое число в заданном диапазоне включая обе границы
- */
-function getRandomInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
